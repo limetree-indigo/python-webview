4b2532070792d8280cebcc452bf083b1706f37d0
homePC <limetree.indigo@gmail.com>
2023-02-16 17:58:39 +0900

    개발중

--- app1.py
+++ app1.py
@@ -1,5 +1,6 @@
 import sys
 
+import schedule
 from PySide6.QtWidgets import QMainWindow, QApplication
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtCore import QUrl
@@ -17,12 +18,19 @@ class Form(QMainWindow):
         self.web.load(QUrl("http://pos.handsorder.com/"))
         self.web.urlChanged.connect(self.changed_url)
         # self.web.loadProgress.connect(self.load_finish)
-        self.web.loadFinished.connect(self.load_finish)
         self.setCentralWidget(self.web)
+        self.web.loadFinished.connect(self.load_finish)
+        
 
     def changed_url(self):
         url = self.web.url().toString()
-        print(url)
+        self.web.page().runJavaScript('localStorage.getItem("shop_cd");', 0, self.change_call_function)
+        self.web.page().runJavaScript('document.querySelector(".flex_div>.posmenu_wr>a:first-child").innerText', 0, self.change_call_function)
+        print("changed url:", url)
+
+    def test(self):
+        print("abc")
+        
 
     def load_finish(self):
         script = """var req = new XMLHttpRequest();
@@ -31,19 +39,26 @@ class Form(QMainWindow):
         var headers = req.getAllRequestHeaders().toLowerCase();
         headers;        
         """
-        self.web.page().runJavaScript(script, 0, self.call_function)
-        self.web.page().runJavaScript('localStorage.getItem("shop_cd");', 1, self.call_function)
-        print("ok")
+        # self.web.page().runJavaScript(script, 0, self.call_function)
+        self.web.page().runJavaScript('localStorage.getItem("shop_cd");', 0, self.load_finish_call_function)
+        self.web.page().runJavaScript('document.querySelector(".flex_div>.posmenu_wr>a:first-child").innerText', 0, self.load_finish_call_function)
+        # print("ok")
 
-    def call_function(self, html):
-        print(html)
-        # print(type(html))
-        # header = html.split('\r\n')
-        # print(header)
+    def load_finish_call_function(self, val):
+        print("load_finish:", val)
+
+    def change_call_function(self, val):
+        print("change:", val)
+
+    
+
+
+        
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     form = Form()
     form.show()
+
     exit(app.exec())
